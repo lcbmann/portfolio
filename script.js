749f8080f03b07836bd04d415ccacda53a956de3
lcbmann <lcbmann44@gmail.com>
2024-09-14 21:58:07 -0700

    Basic portfolio complete!

---
+++ script.js
@@ -0,0 +1,18 @@
+// Navigation Toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navLinksLi = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    // Animate links
+    navLinksLi.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+    // Hamburger animation
+    hamburger.classList.toggle('toggle');
+});
